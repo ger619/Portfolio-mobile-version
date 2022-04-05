10a374d0c596d3761bb1d70483fe6c00902d4a29
David Abol Ger <draker74@gmail.com>
2022-04-05 13:31:58 +0300

    Add a menu

--- style.js
+++ style.js
@@ -1,29 +1,21 @@
-window.onload = function() {
-    const mainMenu = document.querySelector('.mainMenu');
-    const closeMenu = document.querySelector('.closeMenu');
-    const openMenu = document.querySelector('.openMenu');
+window.onload = function openWindow() {
+  const mainMenu = document.querySelector('.mainMenu');
+  const closeMenu = document.querySelector('.closeMenu');
+  const openMenu = document.querySelector('.openMenu');
 
-    function show() {
-        mainMenu.style.display = 'flex';
-        mainMenu.style.top = '0';
-    }
+  function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+  }
 
-    function close() {
-        mainMenu.style.top = '-100%';
-    }
+  function close() {
+    mainMenu.style.top = '-100%';
+  }
 
-    openMenu.addEventListener('click', show);
-    closeMenu.addEventListener('click', close);
+  openMenu.addEventListener('click', show);
+  closeMenu.addEventListener('click', close);
 
-    function redirect() {
-        const location = window.location;
-        const initial = location.pathname;
-        console.log(location);
-    }
-    mainMenu.forEach((link) => {
-        link.addEventListener('click', function() {
-            ul.style.display = "none";
-            reload();
-        });
-    });
+  mainMenu.addEventListener('click', () => {
+    mainMenu.style.display = 'none';
+  });
 };
