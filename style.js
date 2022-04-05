3b005ee0af461c98d1e06d6ade269ca3e2b73f6e
David Abol Ger <draker74@gmail.com>
2022-04-05 11:58:43 +0300

    Menu Add

--- style.js
+++ style.js
@@ -1,32 +1,29 @@
-window.onload = function () {
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+window.onload = function() {
+    const mainMenu = document.querySelector('.mainMenu');
+    const closeMenu = document.querySelector('.closeMenu');
+    const openMenu = document.querySelector('.openMenu');
 
-function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
+    function show() {
+        mainMenu.style.display = 'flex';
+        mainMenu.style.top = '0';
+    }
 
-function close() {
-    mainMenu.style.top = '-100%';
-}
+    function close() {
+        mainMenu.style.top = '-100%';
+    }
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
+    openMenu.addEventListener('click', show);
+    closeMenu.addEventListener('click', close);
 
-function redirect(){
-    let location = window.location;
-    let initial = location.pathname;
-    console.log(location)
-
-}
-
-mainMenu.forEach((link)=> {
-    link.addEventListener('click',function(e){
-        // e.preventDefault();
-div.style.display="none";
-reload();
-    })
- })
-}
+    function redirect() {
+        let location = window.location;
+        let initial = location.pathname;
+        console.log(location);
+    }
+    mainMenu.forEach((link) => {
+        link.addEventListener('click', function(e) {
+            div.style.display = "none";
+            reload();
+        });
+    });
+};
